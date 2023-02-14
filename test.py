a2e07895b9f622431bc50deb499821da4fce5f84
HtwoOtwo <765724965@qq.com>
2023-02-14 17:26:36 +0800

    BaseDT v0.0.1

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from BaseDT.data import ImageData
+from BaseDT.io import *
+import matplotlib.pyplot as plt
 from BaseDT.plot import imshow_det_bboxes
 import numpy as np
 # import mmcv
@@ -7,9 +9,10 @@ import numpy as np
 
 if __name__ == "__main__":
     #img = cv2.imread("D:\PythonProject\OpenDataLab-Edu\dataset\cat2.jpg")
-    img = r"D:\PythonProject\OpenDataLab-Edu\dataset\cat2.jpg"
-    data = ImageData(img, size=(256, 256), crop_size=(224,224))
-    imshow_det_bboxes(img, np.array([[56,56,256,256,1]]),np.array(["car"]))
+    # img = r"D:\PythonProject\OpenDataLab-Edu\dataset\cat2.jpg"
+    # data = ImageData(img, size=(256, 256), crop_size=(224,224))
+    # data.show()
+    #imshow_det_bboxes(img, [[56,56,256,256,1]],[0], ["cat"], 0.5)
     # print(data.value.shape)
     # print(data.raw_value.shape)
     # tensor_value = data.to_tensor()
@@ -17,18 +20,6 @@ if __name__ == "__main__":
     # texts = {'city': 'Dubai', 'temperature': 33},
     # data = TextData(texts, vectorize = True)
     # print(data.value)
-# img = r"D:\PythonProject\OpenDataLab-Edu\dataset\cat2.jpg"
-# data = ImageData(img, size=(256, 256), crop_size=(224,224))
-# input_data = np.array()
-#
-#
-# img_data = input_data.astype('float32')
-# mean_vec = np.array([0.485, 0.456, 0.406])
-# stddev_vec = np.array([0.229, 0.224, 0.225])
-# norm_img_data = np.zeros(img_data.shape).astype('float32')
-# for i in range(img_data.shape[0]):
-#     norm_img_data[i, :, :] = (img_data[i, :, :] / 255 - mean_vec[i]) / stddev_vec[i]
-# norm_img_data = norm_img_data.reshape(1, 3, 224, 224).astype('float32')
-#
-# norm_img_data = ImageData(input_data, size=(256, 256), crop_size=(224,224))
-
+    mic = MicroPhone()
+    plt.plot(mic.record_audio())
+    plt.show()
